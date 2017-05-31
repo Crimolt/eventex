5f24c7739f87eea4fe657335f2eaf66f1decc49e
Crimolt <leviplj@gmail.com>
2017-05-31 00:13:45 -0300

    Adicionando teste automatizado ao salvar arquivos

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+//gruntfile.js
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    watch: {
+      files: ['Gruntfile.js', '**/*.py'],
+      tasks: ['shell']
+    },
+    shell: {
+      options: {
+        execOptions: {
+			    shell: '/bin/bash'
+        }
+		  },
+      test: {
+        command: 'source .wttd/bin/activate && python manage.py test'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-shell');
+
+  grunt.registerTask('default', ['watch']);
+};
